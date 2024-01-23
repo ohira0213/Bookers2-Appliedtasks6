8845da4890e448e3d5b001ed8ac4ed85e8e102e7
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-01-23 10:01:40 +0000

    Bookers2-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books, only: [:create, :index, :show]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
