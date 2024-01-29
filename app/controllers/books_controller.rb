d4813964977fc9bd55c412936fd272fccf2d06b4
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-01-29 11:43:02 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,42 +1,61 @@
 class BooksController < ApplicationController
-  
-  def index
-    @book = Book.new
+
+def index
+  @book = Book.new
+  @books = Book.all
+  @user = current_user
+end
+
+def create
+  @book = Book.new(book_params)
+  @book.user_id = current_user.id
+  @user = current_user
+  if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book.id)
+  else
     @books = Book.all
+    render :index
   end
-  
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-  end
+end
 
   def show
-    @book=Book.find(params[:id])  
-  end
-  
-  def edit
-    @book=Book.find(params[:id])  
-  end
-  
-  def update
-    @book=Book.find(params[:id])  
-    @book.update(book_params)
-    redirect_to book_path
+    @new_book=Book.new
+    @book=Book.find(params[:id])
+    @user=@book.user
   end
 
-  
+  def edit
+    @book=Book.find(params[:id])
+    if @book.user == current_user
+    render :edit 
+    else
+    redirect_to books_path
+    end
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path
+    else
+    @books = Book.all
+    render :edit
+    end
+  end
+
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
 
-  
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body,:profile_image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  get 'home/about' => 'homes#about', as: 'about'
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy, :new]
+  resources :users, only: [:index, :show, :edit, :update]
+  #resources :user_registrations
+  #resources :user_sessions
 end
