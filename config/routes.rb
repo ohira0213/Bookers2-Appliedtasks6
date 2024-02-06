3de4e65279f953cba08c797312153ff95ce451d5
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-02-06 15:28:37 +0000

    Bookers2-Appliedtasks5

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/about' => 'homes#about', as: 'about'
-  
+
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy, :new] do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
       get "followings" => "relationships#followings", as: "followings"
       get "followers" => "relationships#followers", as: "followers"
   end
+
+  get "search" => "searches#search"
 end
