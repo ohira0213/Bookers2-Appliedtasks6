f4970d2a96906b0007218e8da515fff3ab1c51c8
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-02-06 05:55:15 +0000

    Bookers2-Appliedtasks4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,37 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, uniqueness: true,
     length: { minimum: 2, maximum: 20 }
   validates :introduction,
     length: {maximum: 50 }
-    
+
   validates :user_id, uniqueness: {scope: :book_id}
 
   def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
